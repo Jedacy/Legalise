65a36559eb5eb98bc8c5468dd4b204aa4bb6a1c7
Jedacy <jedarks.j@gmail.com>
2026-03-25 11:06:36 +0000

    tiredness is an understatement

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from "react";
 
 import { AppShell } from "@/components/layout/AppShell";
 
+export const runtime = 'edge';
+
 export default function Layout({ children }: { children: ReactNode }) {
   return <AppShell>{children}</AppShell>;
 }
